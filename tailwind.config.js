f1704a1fcf9d80d41b4f82247642ee2d4554403b
ayoub ouahid <aouahid123@gmail.com>
2023-11-20 16:41:37 +0100

    section 1 and navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src//*.{html,js}"],
   theme: {
-    extend: {},
-  },
+    extend: {
+      colors: {
+        "primary": "#1C85E8",
+        "secondary": {
+          dark: "#020202",
+          grey: "#B8B8B8"
+        }
+      },
+      fontFamily: {
+        body: ["Montserrat"]
+      },
+      fontSize: {
+        "xs": "1rem",
+        "sm": "1.5rem",
+        "xl": "3rem",
+      },
+      spacing: {
+        128: "32rem",
+        144: "36rem"
+      },
+    margin: {
+      "mediumWrapper": "8rem",
+
+    },
+    padding: {
+      "mediumWrapper": "8rem",
+
+    },
+
+    },
+    },
+
+
   plugins: [],
 }
